3847e5dedf7fd0c6d9a3b3ea969bd6c36f1b9e6f
Nagy Viktor <52136313+nagyviktor@users.noreply.github.com>
2021-09-05 20:19:08 +0200

    IdentityServer

---
+++ Backend/Services/UserIdentity/UserIdentity.DAL/Domain/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UserIdentity.DAL.Domain
+{
+    public class User : IdentityUser
+    {
+    }
+}
---
+++ Backend/Services/UserIdentity/UserIdentity.DAL/UserIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using UserIdentity.DAL.Domain;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserIdentity.DAL
+{
+    public class UserIdentityDbContext : IdentityDbContext<User>
+    {
+        public UserIdentityDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
