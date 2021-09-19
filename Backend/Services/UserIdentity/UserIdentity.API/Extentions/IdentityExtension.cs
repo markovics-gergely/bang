4018ab6551fb64b0820a79bc0519c451a908b267
Nagy Viktor <52136313+nagyviktor@users.noreply.github.com>
2021-09-19 21:09:21 +0200

    Identity works

---
+++ Backend/Services/UserIdentity/UserIdentity.API/Extentions/IdentityExtension.cs
@@ -0,0 +1,91 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using UserIdentity.DAL;
+using UserIdentity.DAL.Domain;
+
+namespace UserIdentity.API.Extentions
+{
+    public static class IdentityExtension
+    {
+        public static void AddIdentityExtensions(this IServiceCollection services, IConfiguration configuration)
+        {        
+            services.AddIdentity<Account, IdentityRole>()
+                .AddEntityFrameworkStores<UserIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+                options.EmitStaticAudienceClaim = true;
+                options.IssuerUri = configuration.GetValue<string>("Authentication:Authority");
+            })
+                .AddDeveloperSigningCredential()
+                .AddInMemoryPersistedGrants()
+                .AddInMemoryIdentityResources(GetIdentityResources(configuration))
+                .AddInMemoryApiScopes(GetApiScopes(configuration))
+                .AddInMemoryClients(GetClients(configuration))
+                .AddAspNetIdentity<Account>();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.Authority = configuration.GetValue<string>("Authentication:Authority");
+                options.Audience = configuration.GetValue<string>("Authentication:Authority") + "/resources";
+                options.RequireHttpsMetadata = false;
+            });
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources(IConfiguration configuration)
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+        public static IEnumerable<ApiScope> GetApiScopes(IConfiguration configuration)
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope(configuration.GetValue<string>("IdentityServer:Name"), configuration.GetValue<string>("IdentityServer:Description"), 
+                    new string[] {
+                        ClaimTypes.NameIdentifier,
+                        JwtClaimTypes.Name,
+                        ClaimTypes.Role
+                    }
+                )
+            };
+        }
+        public static IEnumerable<Client> GetClients(IConfiguration configuration)
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = configuration.GetValue<string>("IdentityServer:ClientId"),
+                    ClientSecrets = { new Secret(configuration.GetValue<string>("IdentityServer:ClientSecret").Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes = { configuration.GetValue<string>("IdentityServer:Name"), IdentityServerConstants.StandardScopes.OfflineAccess },
+                    AllowOfflineAccess = true,
+                    RefreshTokenExpiration = TokenExpiration.Sliding,
+                    AlwaysSendClientClaims = true
+                }
+            };
+        }
+    } 
+}
--- Backend/Services/UserIdentity/UserIdentity.API/Startup.cs
+++ Backend/Services/UserIdentity/UserIdentity.API/Startup.cs
@@ -1,36 +1,21 @@
-using UserIdentity.BLL.Application.Commands.Commands;
-using UserIdentity.BLL.Application.Commands.Handlers;
-using UserIdentity.BLL.Application.Exceptions;
-using UserIdentity.BLL.Application.Interfaces;
-using UserIdentity.BLL.Application.MappingProfiles;
-using UserIdentity.BLL.Infrastructure.Queries.Handlers;
-using UserIdentity.BLL.Infrastructure.Queries.Queries;
-using UserIdentity.BLL.Infrastructure.Stores;
-using UserIdentity.BLL.Infrastructure.Queries.ViewModels;
+using UserIdentity.API.Extentions;
 using UserIdentity.DAL;
-using UserIdentity.DAL.Domain;
 
+using System;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using System.Reflection;
 
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 using Hellang.Middleware.ProblemDetails;
 using MediatR;
-
-using System.Linq;
-using NSwag;
-using NSwag.Generation.Processors.Security;
-using NSwag.AspNetCore;
-using IdentityServer4.Configuration;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.AspNetCore.Authorization;
 
 namespace UserIdentity.API
 {
@@ -45,82 +30,20 @@ namespace UserIdentity.API
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<UserIdentityDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<UserIdentityDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );    
 
-            services.AddScoped<IAccountStore, AccountStore>();
-            services.AddScoped<IFriendStore, FriendStore>();
-            services.AddScoped<ILobbyStore, LobbyStore>();
+            services.AddAutoMapperExtensions();
+            services.AddExceptionExtensions();
+            services.AddIdentityExtensions(Configuration);
+            services.AddServiceExtensions();
+            services.AddSwaggerExtension(Configuration);
 
-            services.AddScoped<IRequestHandler<CreateAccountCommand, bool>, AccountCommandHandler>();
-            services.AddScoped<IRequestHandler<LoginAccountCommand, Unit>, AccountCommandHandler>();
-            services.AddScoped<IRequestHandler<DeleteAccountCommand, Unit>, AccountCommandHandler>();
-
-            services.AddScoped<IRequestHandler<GetFriendsQuery, IEnumerable<FriendViewModel>>, FriendQueryHandler>();
-            services.AddScoped<IRequestHandler<GetUnacceptedFriendsQuery, IEnumerable<FriendViewModel>>, FriendQueryHandler>();
-            services.AddScoped<IRequestHandler<CreateFriendCommand, Unit>, FriendCommandHandler>();
-            services.AddScoped<IRequestHandler<DeleteFriendCommand, Unit>, FriendCommandHandler>();
-
-            services.AddScoped<IRequestHandler<GetLobbyAccountsQuery, IEnumerable<LobbyAccountViewModel>>, LobbyQueryHandler>();
-            services.AddScoped<IRequestHandler<GetLobbyAccountsQuery, IEnumerable<LobbyAccountViewModel>>, LobbyQueryHandler>();
-            services.AddScoped<IRequestHandler<CreateLobbyAccountCommand, Unit>, LobbyCommandHandler>();
-            services.AddScoped<IRequestHandler<DeleteLobbyAccountCommand, Unit>, LobbyCommandHandler>();
-            services.AddScoped<IRequestHandler<CreateLobbyCommand, string>, LobbyCommandHandler>();
-            services.AddScoped<IRequestHandler<DeleteLobbyCommand, Unit>, LobbyCommandHandler>();
-
-            services.AddMvc();
             services.AddMediatR(typeof(Startup).GetTypeInfo().Assembly);
+            services.AddSignalR();
 
             services.AddControllers();
-
-            services.AddAutoMapper(typeof(AccountProfile));
-            services.AddAutoMapper(typeof(FriendProfile));
-            services.AddAutoMapper(typeof(LobbyProfile));
-
-            services.AddHttpContextAccessor();
-
-            services.AddIdentity<Account, IdentityRole>()
-                .AddEntityFrameworkStores<UserIdentityDbContext>()
-                .AddDefaultTokenProviders();
-
-            services.AddIdentityServer(options =>
-            {
-                options.UserInteraction = new UserInteractionOptions()
-                {
-                    LoginUrl = "/Account/signin",
-                    LogoutUrl = "/Account/signout",
-
-                    LoginReturnUrlParameter = "returnUrl"
-                };
-                options.Authentication.CookieAuthenticationScheme = IdentityConstants.ApplicationScheme;
-            })
-            .AddDeveloperSigningCredential()
-            .AddInMemoryPersistedGrants()
-            .AddInMemoryIdentityResources(Configuration.GetSection("IdentityServer:IdentityResources"))
-            .AddInMemoryApiResources(Configuration.GetSection("IdentityServer:ApiResources"))
-            .AddInMemoryApiScopes(Configuration.GetSection("IdentityServer:ApiScopes"))
-            .AddInMemoryClients(Configuration.GetSection("IdentityServer:Clients"))
-            .AddAspNetIdentity<Account>();
-
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("api-openid", policy => policy.RequireAuthenticatedUser()
-                    .RequireClaim("scope", "api-openid")
-                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme));
-
-                options.DefaultPolicy = options.GetPolicy("api-openid");
-            });
-
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.Authority = Configuration.GetValue<string>("Authentication:Authority");
-                options.Audience = Configuration.GetValue<string>("Authentication:Audience");
-                options.RequireHttpsMetadata = false;
-            });
 
             services.AddCors(options =>
             {
@@ -132,73 +55,24 @@ namespace UserIdentity.API
                            .AllowCredentials();
                 });
             });
-
-            services.AddProblemDetails(options =>
-            {
-                options.IncludeExceptionDetails = (ctx, ex) => false;
-                options.Map<EntityNotFoundException>(
-                (ctx, ex) =>
-                {
-                    var pd = StatusCodeProblemDetails.Create(StatusCodes.Status404NotFound);
-                    pd.Title = ex.Message;
-                    return pd;
-                });
-                options.Map<InvalidParameterException>(
-                (ctx, ex) =>
-                {
-                    var pd = StatusCodeProblemDetails.Create(StatusCodes.Status401Unauthorized);
-                    pd.Title = ex.Message;
-                    return pd;
-                });
-            });
-
-            services.AddOpenApiDocument(document =>
-            {
-                document.Title = "User Identity";
-                document.AddSecurity("Bearer", Enumerable.Empty<string>(), new OpenApiSecurityScheme
-                {
-                    Type = OpenApiSecuritySchemeType.OAuth2,
-                    Flow = OpenApiOAuth2Flow.Implicit,
-                    Flows = new OpenApiOAuthFlows()
-                    {
-                        AuthorizationCode = new OpenApiOAuthFlow
-                        {
-                            AuthorizationUrl = $"{Configuration.GetValue<string>("Authentication:Authority")}/connect/authorize",
-                            TokenUrl = $"{Configuration.GetValue<string>("Authentication:Authority")}/connect/token",
-                            Scopes = new Dictionary<string, string>
-                            {
-                                { "openid", "OpenId" },
-                                { "api-openid", "all" }
-                            }
-                        }
-                    }
-                });
-                document.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("Bearer"));
-            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseProblemDetails();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserIdentity v1");
+                c.OAuthConfigObject.ClientId = Configuration.GetValue<string>("IdentityServer:ClientId");
+                c.OAuthConfigObject.ClientSecret = Configuration.GetValue<string>("IdentityServer:ClientSecret");
+            });
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            app.UseCors("Cors");
-
-            app.UseOpenApi();
-            app.UseSwaggerUi3(config =>
-            {
-                config.OAuth2Client = new OAuth2ClientSettings
-                {
-                    ClientId = "useridentity-swagger",
-                    ClientSecret = null,
-                    UsePkceWithAuthorizationCodeGrant = true,
-                    ScopeSeparator = " ",
-                    Realm = null,
-                    AppName = "User Identity"
-                };
-            });
+            app.UseCors();
 
             app.UseAuthentication();
             app.UseIdentityServer();
